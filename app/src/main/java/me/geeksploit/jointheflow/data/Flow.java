def7cce63b7c17d4b9897f04421de3f53753ead2
Max Rem <max@geeksploit.me>
2018-08-17 21:17:08 +0300

    feat: Add Flow POJO

---
+++ app/src/main/java/me/geeksploit/jointheflow/data/Flow.java
@@ -0,0 +1,17 @@
+package me.geeksploit.jointheflow.data;
+
+public final class Flow {
+
+    private String title;
+
+    public Flow() {
+    }
+
+    public Flow(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
