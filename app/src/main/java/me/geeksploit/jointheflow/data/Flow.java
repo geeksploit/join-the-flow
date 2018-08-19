cc5de1d2b5854cad5d902e33dda99856cfc95f4e
Max Rem <max@geeksploit.me>
2018-08-19 18:44:18 +0300

    feat: Add a field to store joined users count

--- app/src/main/java/me/geeksploit/jointheflow/data/Flow.java
+++ app/src/main/java/me/geeksploit/jointheflow/data/Flow.java
@@ -3,15 +3,21 @@ package me.geeksploit.jointheflow.data;
 public final class Flow {
 
     private String title;
+    private long joinedCount;
 
     public Flow() {
     }
 
     public Flow(String title) {
         this.title = title;
+        this.joinedCount = joinedCount;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public long getJoinedCount() {
+        return joinedCount;
     }
 }
