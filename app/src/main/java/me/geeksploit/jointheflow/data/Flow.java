e9a3c95b08ba209d5228d474b0bf15d9f363cabc
Max Rem <max@geeksploit.me>
2018-08-19 18:52:00 +0300

    feat: Consider flows to be equal when their titles match

--- app/src/main/java/me/geeksploit/jointheflow/data/Flow.java
+++ app/src/main/java/me/geeksploit/jointheflow/data/Flow.java
@@ -20,4 +20,18 @@ public final class Flow {
     public long getJoinedCount() {
         return joinedCount;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        if (! (other instanceof Flow)) return false;
+
+        Flow otherFlow = (Flow) other;
+        return this.getTitle().equals(otherFlow.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return getTitle().hashCode();
+    }
 }
