db6f7fec386d62105d6ec6d9ed45e3219e10be5f
Max Rem <max@geeksploit.me>
2018-08-20 20:55:25 +0300

    style: Rearrange imports

--- app/src/main/java/me/geeksploit/jointheflow/FlowListActivity.java
+++ app/src/main/java/me/geeksploit/jointheflow/FlowListActivity.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,12 +25,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import me.geeksploit.jointheflow.data.Flow;
-import me.geeksploit.jointheflow.dummy.DummyContent;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import me.geeksploit.jointheflow.data.Flow;
+import me.geeksploit.jointheflow.dummy.DummyContent;
 
 /**
  * An activity representing a list of Flows. This activity
