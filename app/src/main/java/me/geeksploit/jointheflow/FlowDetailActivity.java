8818ef1cfd95477f561cccc63bf8ee8edd758a70
Max Rem <max@geeksploit.me>
2018-08-27 06:46:24 +0300

    feat: Allow join and leave a flow from detail activity

--- app/src/main/java/me/geeksploit/jointheflow/FlowDetailActivity.java
+++ app/src/main/java/me/geeksploit/jointheflow/FlowDetailActivity.java
@@ -2,14 +2,21 @@ package me.geeksploit.jointheflow;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * An activity representing a single Flow detail screen. This
@@ -19,6 +26,9 @@ import android.view.MenuItem;
  */
 public class FlowDetailActivity extends AppCompatActivity {
 
+    private String mFlowId;
+    private DatabaseReference mFlowsDatabaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +36,29 @@ public class FlowDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
+        mFlowsDatabaseReference = FirebaseDatabase.getInstance().getReference().child("flows");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final String userId =  FirebaseAuth.getInstance().getUid();
+                mFlowsDatabaseReference.child(mFlowId).child("joined").child(userId)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            leaveTheFlow(mFlowId, userId);
+                        } else {
+                            joinTheFlow(mFlowId, userId);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
 
@@ -50,12 +77,12 @@ public class FlowDetailActivity extends AppCompatActivity {
         //
         // http://developer.android.com/guide/components/fragments.html
         //
+        mFlowId = getIntent().getStringExtra(FlowDetailFragment.ARG_ITEM_ID);
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
             Bundle arguments = new Bundle();
-            arguments.putString(FlowDetailFragment.ARG_ITEM_ID,
-                    getIntent().getStringExtra(FlowDetailFragment.ARG_ITEM_ID));
+            arguments.putString(FlowDetailFragment.ARG_ITEM_ID, mFlowId);
             FlowDetailFragment fragment = new FlowDetailFragment();
             fragment.setArguments(arguments);
             getSupportFragmentManager().beginTransaction()
@@ -79,5 +106,13 @@ public class FlowDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void joinTheFlow(String flowId, String userId) {
+        mFlowsDatabaseReference.child(flowId).child("joined").child(userId).setValue(System.currentTimeMillis());
+    }
+
+    private void leaveTheFlow(String flowId, String userId) {
+        mFlowsDatabaseReference.child(flowId).child("joined").child(userId).removeValue();
     }
 }
